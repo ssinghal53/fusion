214a731484aa7490bede99651d06b4a6a50264ec
Sharad Singhal <sharad.singhal1@gmail.com>
2020-03-14 20:45:13 -0700

    add docs directory for javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.aifusion.cimserver"},{"l":"net.aifusion.cql"},{"l":"net.aifusion.metamodel"},{"l":"net.aifusion.providers"},{"l":"net.aifusion.utils"}]
