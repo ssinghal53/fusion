1b5ad5491911b4166866246f269b48eb6a28648c
Sharad Singhal <sharad.singhal@hpe.com>
2020-04-29 07:40:47 -0700

    updated package-info to fix non-ascii hyphen

--- src/net/aifusion/asn/package-info.java
+++ src/net/aifusion/asn/package-info.java
@@ -3,15 +3,15 @@ This package contains a partial implementation an ASN.1 parser and classes to ma
 <p><b>Related Documentation</b></p>
 <ol>
 <li><a href="https://www.itu.int/rec/T-REC-X.680-201508-I/en">
-ITU-T : Recommendation X.680 Information technology – Abstract Syntax Notation One (ASN.1): Specification of basic notation, (7/2002)</a></li>
+ITU-T : Recommendation X.680 Information technology - Abstract Syntax Notation One (ASN.1): Specification of basic notation, (7/2002)</a></li>
 <li><a href="https://www.itu.int/rec/T-REC-X.681/en">
 X.681 : Information technology - Abstract Syntax Notation One (ASN.1): Information object specification (7/2202)</a></li>
 <li><a href="https://www.itu.int/rec/T-REC-X.682">
-X.682 : Information technology – Abstract Syntax Notation One (ASN.1): Constraint specification (7/2002)</a></li>
+X.682 : Information technology - Abstract Syntax Notation One (ASN.1): Constraint specification (7/2002)</a></li>
 <li><a href="https://www.itu.int/rec/T-REC-X.683/en">
-X.683 : Information technology – Abstract Syntax Notation One (ASN.1): Parameterization of ASN.1 specifications (7/2002)</a></li>
+X.683 : Information technology - Abstract Syntax Notation One (ASN.1): Parameterization of ASN.1 specifications (7/2002)</a></li>
 <li><a href="https://www.itu.int/rec/T-REC-X.690/en">
-X.690 : Information technology – ASN.1 encoding rules: Specification of Basic Encoding Rules (BER), Canonical Encoding Rules (CER) and Distinguished Encoding Rules (DER)</a></li>
+X.690 : Information technology - ASN.1 encoding rules: Specification of Basic Encoding Rules (BER), Canonical Encoding Rules (CER) and Distinguished Encoding Rules (DER)</a></li>
 
 </ol>
 */
