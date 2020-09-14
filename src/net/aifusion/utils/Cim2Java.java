ea2c90125f7e3d53351b78bcefc405f9a10cb328
Sharad Singhal <sharad.singhal@hpe.com>
2020-09-14 12:50:17 -0700

    update copyright message in Cim2Java

--- src/net/aifusion/utils/Cim2Java.java
+++ src/net/aifusion/utils/Cim2Java.java
@@ -64,6 +64,7 @@ public class Cim2Java {
 	private static String dated = "July 19, 2019";
 	/** Header inserted at the top of the generated java class */
 	private static String copyRightHeader = "/*\n * CIM to Java Stub Generator Version "+version+" dated "+dated+"\n"+
+							" * Copyright (c) Sharad Singhal, 2019\n"+
 							" * Stubs may be used without restriction except for retaining this comment in source code\n */\n";
 	
 	/** Annotations added to every generated java class */
