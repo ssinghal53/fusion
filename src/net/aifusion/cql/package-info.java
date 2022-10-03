fbd05c1016ec6c6ac786641c3e63a2dc00669b45
Sharad Singhal <sharad.singhal@hpe.com>
2022-10-03 15:57:39 -0700

    updated package info for CQL

--- src/net/aifusion/cql/package-info.java
+++ src/net/aifusion/cql/package-info.java
@@ -6,12 +6,20 @@
  * <p> The package exposes a single class
  * {@linkplain CimQuery} that can be used to query instances within a repository.</p>
  * <p>Currently, while the majority of the CQL specification can be parsed as a valid query, the actual query engine is only partially implemented. Thus,
- * mileage on a specific query may vary, especially for queries that involve joins. The Basic Query form defined in DSP0202 should, however, work:
+ * mileage on a specific query may vary, especially for queries that involve joins. The Basic Query form defined in DSP0202 should, however, work:</p>
  * <pre>
  * SELECT * FROM class_name WHERE &lt;selection expression&gt; 
  * </pre>
  * <p>The query will return a list of structure values that meet the desired expression. Aggregation functions (e.g., COUNT, MIN, MAX, SUM, MEAN, MEDIAN) are currently
  * not implemented. However, other result-set operations (i.e., FIRST, DISTINCT, ORDER BY) are implemented.</p>
+ * <p>In addition, the following constructs are implemented:</p>
+ * <pre>
+ * DELETE [FIRST uint] [DISTINCT] FROM className [WHERE searchCondition] [ORDER BY SortList]
+ * UPDATE className SET propertyName1 = value1 [, propertyName2 = value2, ...] [WHERE searchCondition]
+ * </pre>
+ * <p>The DELETE statement deletes the instances where searchCondition is true. If more than one instance matches, a subset can be deleted using the ORDER BY constructs. The UPDATE
+ * construct updates one or more property values in existing instances. Note that in both cases, it is important to include the WHERE clause, else all instances of the class will be
+ * deleted or updated.</p> 
  * <p>In addition, The construct <code>$identifier$</code> can be used in the query to define &quot;delayed constants&quot; that can be set within the query using
  * {@link CimQuery#setVariable(String, net.aifusion.metamodel.DataValue)} after it is parsed. This is useful, for example to reuse the same query with different
  * values in the selection expression without the overhead of re-constructing the query each time.</p>
